a4a6a25ab19651f44e0b9428e124bacc445e8b19
Miguel-mmf <miguel.ferreira@cear.ufpb.br>
2023-05-07 19:58:25 -0300

    Adjusting some details

--- picoSetup.sh
+++ picoSetup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# for official documentation check the following materiial: https://datasheets.raspberrypi.com/pico/getting-started-with-pico.pdf 
-
 # function to build the raspberry pico examples with sdk
 function setup_pico_sdk {
   cd ~/ && mkdir pico && cd pico && echo "STEP 1 DONE" &&\
@@ -14,21 +12,24 @@ function setup_pico_sdk {
   cd ~/pico/pico-examples && echo "STEP 11 DONE" && mkdir build && echo "STEP 12 DONE" &&\
   cd ~/pico/pico-examples/build && echo "STEP 13 DONE" &&\
   export PICO_SDK_PATH=~/pico/pico-sdk && cd ~/pico/pico-examples/build && cmake .. && echo "STEP 14 DONE" &&\
-  cd ~/pico/pico-examples/build && make -j8 && echo "STEP 16 DONE"
+  cd ~/pico/pico-examples/build && make -j8 && echo "STEP 15 DONE"
 }
 
-function setup_pico_sdk {
+function setup_freertos {
   cd ~/pico/ && git clone https://github.com/FreeRTOS/FreeRTOS-Kernel --branch smp --depth 1 && echo "STEP 1 DONE" &&\
   cd ~/pico/pico-examples && export FREERTOS_KERNEL_PATH=~/pico/FreeRTOS-Kernel &&\
-  cd ~/pico/pico-examples/build/pico-w &&\
+  cd ~/pico/pico-examples/build/pico_w &&\
   cmake -DPICO_BOARD=pico_w -DPICO_SDK_PATH=~/pico/pico-sdk -DFREERTOS_KERNEL_PATH=$HOME/pico/FreeRTOS-Kernel -DWIFI_SSID=LEAD -DWIFI_PASSWORD=LEADLEAD .. &&\
   cd ~/pico/pico-examples/build/pico_w/wifi/blink # at this point the .elf2 should be available already ;)
 }
 
 function install_opencd {
+  # Failed to clone 'src/jtag/drivers/libjaylink' a second time, aborting
+    # https://forums.raspberrypi.com/viewtopic.php?t=326692
+    # 
   sudo apt install automake autoconf build-essential texinfo libtool libftdi-dev libusb-1.0-0-dev -y && echo "STEP 1 DONE" &&\
   cd ~/pico && git clone https://github.com/raspberrypi/openocd.git --branch rp2040 --recursive --depth=1 && echo "STEP 2 DONE" &&\
-  cd ~/pico/openocd && ./bootstrap && ./configure --enable-ftdi --enable-sysfsgpio --enable-bcm2835gpio && make -j4 && sudo make install && echo "STEP 3 DONE"
+  cd ~/pico/openocd && ./bootstrap && ./configure --enable-ftdi --enable-sysfsgpio --enable-bcm2835gpio && make -j4 && sudo make install -y && echo "STEP 3 DONE"
 }
 
 function debug_with_swd {
@@ -57,7 +58,27 @@ function picoprobe_setup {
 }
 
 while true; do
+  
+  echo "
+  ______  ______  ______  ______  ______  ______  ______  ______  ______  ______  ______ 
+ |______||______||______||______||______||______||______||______||______||______||______|
+   ____          _        _      _____  _                _____        _                  
+  / __ \        (_)      | |    |  __ \(_)              / ____|      | |                 
+ | |  | | _   _  _   ___ | | __ | |__) |_   ___  ___   | (___    ___ | |_  _   _  _ __   
+ | |  | || | | || | / __|| |/ / |  ___/| | / __|/ _ \   \___ \  / _ \| __|| | | || '_ \  
+ | |__| || |_| || || (__ |   <  | |    | || (__| (_) |  ____) ||  __/| |_ | |_| || |_) | 
+  \___\_\ \__,_||_| \___||_|\_\ |_|    |_| \___|\___/  |_____/  \___| \__| \__,_|| .__/  
+                                                                                 | |     
+                                                                                 |_|     
+  ______  ______  ______  ______  ______  ______  ______  ______  ______  ______  ______ 
+ |______||______||______||______||______||______||______||______||______||______||______|"
+  echo "If you are developing for Raspberry Pi Pico, most of the installation steps in the Getting
+Started guide can be accomplished by executing this script. For official documentation, please
+consult the following: https://datasheets.raspberrypi.com/pico/getting-started-with-pico.pdf
+  
+  "
   echo "Please choose an option:"
+  echo "0. Exit"
   echo "1. Setup Raspberry Pico in SDK mode"
   echo "2. Setup Raspberry Pico alongside FreeRTOS"
   echo "3. Install OpenCD"
@@ -67,8 +88,9 @@ while true; do
   read -p "Enter your choice: " choice
 
   case $choice in
+    0) break ;;
     1) setup_pico_sdk; break ;;
-    2) setup_pico_sdk; break ;;
+    2) setup_freertos; break ;;
     3) install_opencd; break ;;
     4) debug_with_swd; break ;;
     5) debug_with_vscode; break;;
